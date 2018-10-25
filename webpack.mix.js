4a883fc8dcbbcc15b30dc08a92fcb185e2cacb71
edusito <43457188+edusito@users.noreply.github.com>
2018-10-25 17:45:25 -0500

    https://github.com/edusito/Lab3.git

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,20 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    
-    'resources/plantillas/css/boostrap.css',
-    'resources/plantillas/css/font-awesome.css',
-    'resources/plantillas/css/ionicosns.css',
-    'resources/plantillas/css/jquery-ui.css',
-    'resources/plantillas/css/style.css',
-    
-    'public/Asset/css/plantilla.css'
-
-
    .sass('resources/sass/app.scss', 'public/css');
-   
-   'resources/plantillas/Asset/css/jquery-ui.css',
-   'resources/plantillas/Asset/css/jquery-ui.css',
-   'resources/plantillas/Asset/css/jquery-ui.css',
-   'public/js/plantillas.js'
-   
