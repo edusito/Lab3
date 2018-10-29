64729de1302dc1389ed5ffc3d5eaccebd4bbca48
edusito <43457188+edusito@users.noreply.github.com>
2018-10-29 13:39:10 -0500

    https://github.com/edusito/Lab3.git

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,39 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+/*'resources/js/plugin/imagesloaded.pkgd.min.js',
+'resources/js/plugin/isotope.pkgd.min.js',
+'resources/js/plugin/jquery.colorbox-minjs',
+'resources/js/plugin/jquery.easing.js',
+'resources/js/plugin/jquery.fitvids.js',
+'resources/js/plugin/jquery.flexslider.js',
+'resources/js/plugin/jquery.fs.tipper.min.js',
+'resources/js/plugin/jquery.stellar.min.js',
+'resources/js/plugin/jquery.viewportchecker.js',
+'resources/js/plugin/masonry.pkgd.min.js',
+'resources/js/plugin/mediaelement-and-player.min.js',
+'resources/js/plugin/owl.carousel.min.js',
+'resources/js/plugin/wow.min.js',
+
+'resources/js/app.js',
+'resources/js/bootstrap.js',
+'resources/js/bootstrap.min.js',
+'resources/js/jquery-1.11.2.min.js',
+'resources/js/jquery-ui.min.js',
+'resources/js/theme.js',*/
+
+
    .sass('resources/sass/app.scss', 'public/css');
+
+/*'resources/sass/plugin/animate.css',
+'resources/sass/plugin/colorbox.css',
+'resources/sass/plugin/flexslider.css',
+'resources/sass/plugin/jquery.fs.tipper.css',
+'resources/sass/plugin/mediaelementplayer.css',
+'resources/sass/plugin/owl.carousel.css';
+
+'resources/sass/bootstrap.css',
+'resources/sass/font-awesome.css',
+'resources/sass/ionicons.css',
+'resources/sass/jquery-ui.css',
+'resources/sass/style.css';*/
